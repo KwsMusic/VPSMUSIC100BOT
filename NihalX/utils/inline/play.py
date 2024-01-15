a3a91edc14c63782652158cd8cc6145559a147e3
UNKNOWN-MUSIC <130531308+KwsMusic@users.noreply.github.com>
2024-01-15 14:58:37 +0530

    Update play.py

--- NihalX/utils/inline/play.py
+++ NihalX/utils/inline/play.py
@@ -40,7 +40,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 callback_data=f"add_playlist {videoid}",
             ),
             InlineKeyboardButton(
-                text="✨ 𝐎ωиєя ⏎", url=f"https://t.me/L2R_KING0"
+                text="✨ 𝐎ωиєя ⏎", url=f"OWNER_ID"
             ),
         ],
         [
@@ -68,7 +68,7 @@ def telegram_markup_timer(_, chat_id, played, dur):
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             ),
-            InlineKeyboardButton(text="✨ 𝐎ωиєя ⏎", url=f"https://t.me/L2R_KING0"),
+            InlineKeyboardButton(text="✨ 𝐎ωиєя ⏎", url=f"OWNER_ID"),
         ],
         [
             InlineKeyboardButton(
@@ -91,7 +91,7 @@ def stream_markup(_, videoid, chat_id):
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             ),
-            InlineKeyboardButton(text="✨ 𝐎ωиєя ⏎", url=f"https://t.me/L2R_KING0"),
+            InlineKeyboardButton(text="✨ 𝐎ωиєя ⏎", url=f"OWNER_ID"),
         ],
         [
             InlineKeyboardButton(
